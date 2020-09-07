82f4de6cb495898e0d9917375fbd520feec8a732
ArtcalO97 <niyca14@gmail.com>
2020-09-07 23:38:07 +0200

    Updating settings for mail send use

--- converter/settings.py
+++ converter/settings.py
@@ -123,3 +123,14 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
+#gmail_send/settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'yoorusername@gmail.com'
+EMAIL_HOST_PASSWORD = 'key' #past the key or password app here
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'default from email'
+
