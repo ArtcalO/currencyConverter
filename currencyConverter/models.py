93113d3f24464adc5dbab8fc2f1187318568564d
ArtcalO <niyca14@gmail.com>
2021-02-06 17:58:26 +0100

    resolve tracking issue

--- currencyConverter/models.py
+++ currencyConverter/models.py
@@ -19,42 +19,42 @@ class TransactionPercent(models.Model):
 	def __str__(self):
 		return f"Le taux de transaction : {self.trans_percent}"
 
-# class Tracking(models.Model):
-# 	currency_in = models.ForeignKey(Country, related_name='curr_in', on_delete=models.CASCADE)
-# 	currency_out = models.ForeignKey(Country, related_name='curr_out', on_delete=models.CASCADE)
-# 	amount_in = models.PositiveIntegerField()
-# 	amount_out = models.PositiveIntegerField()
+class Tracking(models.Model):
+	currency_in = models.ForeignKey(Country, related_name='curr_in', on_delete=models.CASCADE)
+	currency_out = models.ForeignKey(Country, related_name='curr_out', on_delete=models.CASCADE)
+	amount_in = models.PositiveIntegerField()
+	amount_out = models.PositiveIntegerField()
 
-# 	# fielsd for sender
+	# fielsd for sender
 
-# 	name_sender = models.CharField(max_length=100)
-# 	subname_sender = models.CharField(max_length=100)
-# 	email_sender = models.EmailField()
-# 	phone_sender = models.CharField(max_length=100)
+	name_sender = models.CharField(max_length=100)
+	subname_sender = models.CharField(max_length=100)
+	email_sender = models.EmailField()
+	phone_sender = models.CharField(max_length=100)
 
-# 	# fields for reciever
+	# fields for reciever
 
-# 	name_reciever = models.CharField(max_length=100)
-# 	subname_reciever = models.CharField(max_length=100)
-# 	email_reciever = models.EmailField()
-# 	phone_reciever = models.CharField(max_length=100)
+	name_reciever = models.CharField(max_length=100)
+	subname_reciever = models.CharField(max_length=100)
+	email_reciever = models.EmailField()
+	phone_reciever = models.CharField(max_length=100)
 
-# 	# Livraison 
+	# Livraison 
 
-# 	lumicash = models.IntegerField(null=True, blank=True)
-# 	ecocash = models.IntegerField(null=True, blank=True)
+	lumicash = models.IntegerField(null=True, blank=True)
+	ecocash = models.IntegerField(null=True, blank=True)
 
-# 	tel_livraison = models.IntegerField(null=True, blank=True)
-# 	domicile_livraison = models.CharField(max_length=100, blank=True)
+	tel_livraison = models.IntegerField(null=True, blank=True)
+	domicile_livraison = models.CharField(max_length=100, blank=True)
 
-# 	account_number = models.IntegerField(null=True, blank=True)
-# 	account_holder = models.CharField(max_length=100)
-# 	bank_name = models.CharField(max_length=150)
+	account_number = models.IntegerField(null=True, blank=True)
+	account_holder = models.CharField(max_length=100)
+	bank_name = models.CharField(max_length=150)
 
-# 	validated1 = models.BooleanField(default=False)
-# 	validated2 = models.BooleanField(default=False)
+	validated1 = models.BooleanField(default=False)
+	validated2 = models.BooleanField(default=False)
 
 
-# 	def __str__(self):
-# 		return self.name_sender+' to '+self.name_reciever
+	def __str__(self):
+		return self.name_sender+' to '+self.name_reciever
 
